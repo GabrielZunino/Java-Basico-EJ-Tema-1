43165865245e0e3d7335edb14063a323ae3f69b7
GabrielZunino <115326688+GabrielZunino@users.noreply.github.com>
2022-10-23 01:11:54 -0300

    Add files via upload

---
+++ JavaBasiEjT1.java
@@ -0,0 +1,36 @@
+package com.javabasicoEjT1;
+
+public class JavaBasiEjT1  {
+
+    /*
+    Primer trabajo de presentacion Curso Java Basico
+
+            Crea un proyecto de Java desde 0
+
+            Dentro del proyecto tenéis que crear un paquete. En el paquete tendréis que crear una clase.
+
+            Dentro de la clase tenéis que crear el método main e imprimir todos los datos que se han visto en las sesiones.
+
+        Recordatorio: Los tipos de datos más comunes son int, long, double, boolean, String.
+     */
+
+    public static void main(String[] args) {
+
+        int numeroentero = 10;
+        long numerolargo = (long) 123.22;
+        double numerodecimal = 55.21d;
+        boolean estadologico = true;
+        String texto1 = "Primer presentacion Java";
+
+        // impresion
+
+        System.out.println("imprime int " + numeroentero);
+        System.out.println("imprime long " + numerolargo);
+        System.out.println("imprime double " + numerodecimal);
+        System.out.println("imprime Boolean " + estadologico);
+        System.out.println("imprime String " + texto1 );
+
+
+
+    }
+}
